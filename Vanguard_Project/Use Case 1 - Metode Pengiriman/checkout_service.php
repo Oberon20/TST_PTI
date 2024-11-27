efb0cc05d75bec2f9168aa5a86ec92e376c94451
Muhammad Luthfi Zahran Elwino <133186338+Oberon20@users.noreply.github.com>
2024-11-28 06:55:09 +0700

    Create checkout_service.php

---
+++ Vanguard_Project/Use Case 1 - Metode Pengiriman/checkout_service.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+class CheckoutServiceCopy {
+    protected $shippingMethod = null;
+
+    /**
+     * Mengatur metode pengiriman untuk checkout.
+     * @param string $shippingMethod
+     * @return string
+     */
+    public function setShippingMethod($shippingMethod) {
+        $allowedMethods = ['Same-Day', 'Express', 'Longer'];
+        if (!in_array($shippingMethod, $allowedMethods)) {
+            return "Metode pengiriman tidak valid.";
+        }
+        $this->shippingMethod = $shippingMethod;
+        return "Metode pengiriman telah diatur ke " . $shippingMethod;
+    }
+
+}
